2f441e36ad40f5f866752a437d5fa1a8dc53bfd1
Victoria Voytenko <vvoytenk@student.unit.ua>
2019-09-29 17:42:16 +0300

    get token

--- SwiftyCompanion/ViewController.swift
+++ SwiftyCompanion/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     
@@ -47,6 +49,7 @@ class ViewController: UIViewController {
     }
     
     func validation() -> Bool {
+        requestManager()
         return false
     }
     
@@ -60,6 +63,49 @@ class ViewController: UIViewController {
     
     @objc func keybordIsHiden(notification : NSNotification) {
         scrollView.setContentOffset(CGPoint.init(x:0, y:0), animated: true)
+    
     }
+    
+    struct TokenResponse: Decodable {
+        var identity: String
+        var token: String
+    }
+    
+    func requestManager() {
+        
+        let key = "0b5c29cb49d82af313050fbafa127ab97044364e77072264a7690d8424f3b144"
+        let secret = "b98b0a8b5dee76cb57fe57bf3dee5d0ee2dfc1a30bd5a4bcd1d92b1e35d5d35f"
+        
+        let decoder = JSONDecoder()
+        
+        let url = URL(string: "https://api.intra.42.fr/oauth/token")!
+        
+        var request = URLRequest(url: url)
+        let params = "grant_type=client_credentials client_id=\(key) client_secret=\(secret)"
+        request.httpBody = params.data(using: .utf8, allowLossyConversion: true)
+        request.httpMethod = "POST"
+       // request.addValue("\(key):\(secret)", forHTTPHeaderField: "Authorization")
+      //  var token = ""
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let json = data {
+                do {
+                    let tokenResponse = try decoder.decode(TokenResponse.self, from: json)
+                    print(tokenResponse.identity)
+                    print(tokenResponse.token)
+                } catch {
+                    print(error)
+                }
+            }
+                else { if let error = error {
+                    print(error)
+                    }
+                }
+            }
+            task.resume()
+          //  print(String(data : data, encoding: .utf8)!)
+          //  if let httpResponse = response as? HTTPURLResponse {
+           // token = httpResponse.allHeaderFields["access_token"] as! String
+           // print(token)
+        }
 }
 
